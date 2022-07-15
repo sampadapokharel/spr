b4db7c6ad078972cae0b8ad032774df64a0b9266
sampadapokharel <pokharelsampada@gmail.com>
2022-07-15 00:06:38 -0400

    R ladies R package

---
+++ R/compute_corr.R
@@ -0,0 +1,30 @@
+#' Computes a tidy correlation
+#'
+#' @param data input data set
+#' @param var1 name of variable 1
+#' @param var2 name of variable 2
+#'
+#' @return A tibble with the Pearson correlation and the p-value
+#' @export
+#'
+#' @examples
+#' compute_corr(data = faithful, var1 = eruptions, var2 = waiting)
+#' @importFrom rlang .data
+
+
+compute_corr <- function(data, var1, var2){
+
+  # compute correlation ----
+  cor.test(
+    x = data %>% dplyr::pull({{var1}}),
+    y = data %>% dplyr::pull({{var2}})
+  ) %>%
+    # tidy up results ----
+  broom::tidy() %>%
+    # retain and rename relevant bits ----
+  dplyr::select(
+    correlation = .data$estimate,
+    pval = .data$p.value
+  )
+
+}
